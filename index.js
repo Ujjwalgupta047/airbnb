0cfcecd81f34ab23a0700122719cfa831258d24f
Ujjwalgupta047 <146421055+Ujjwalgupta047@users.noreply.github.com>
2024-06-27 11:31:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const reviewRouter=require("./routes/review.js");
 const userRouter=require("./routes/user.js");
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_CLOUD_NAME,
+    cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET
 });
