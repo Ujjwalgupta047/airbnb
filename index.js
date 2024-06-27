8900914fc764d31fdf01f0cd6c2e9ef6a04108a8
Ujjwalgupta047 <146421055+Ujjwalgupta047@users.noreply.github.com>
2024-06-27 11:25:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ const reviewRouter=require("./routes/review.js");
 const userRouter=require("./routes/user.js");
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: process.env.CLOUD_CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
 });
 //middlewares 
 app.use(express.urlencoded({extended:true}));
